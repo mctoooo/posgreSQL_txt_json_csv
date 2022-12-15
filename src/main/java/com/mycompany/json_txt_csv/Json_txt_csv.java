709a1583bd417da72d851d938b5609b6bfa0687c
Nikolay <mc_an@rambler.ru>
2022-12-15 14:03:46 +0700

    Exception in thread "main" java.lang.ClassFormatError: Duplicate field name "<error>" with signature "Ljava.lang.Object;" in class file com/mycompany/json_txt_csv/Json_txt_csv$1 	at java.base/java.lang.ClassLoader.defineClass1(Native Method)

--- src/main/java/com/mycompany/json_txt_csv/Json_txt_csv.java
+++ src/main/java/com/mycompany/json_txt_csv/Json_txt_csv.java
@@ -13,6 +13,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.logging.Level;
@@ -25,40 +26,41 @@ import java.util.logging.Logger;
 public class Json_txt_csv {
 
     public static void main(String[] args) throws FileNotFoundException, SQLException, IOException {
-        try (Connection conn = DriverManager.getConnection(
-                "jdbc:postgresql://localhost:5432/postgres", "postgres", "mctoooo")) {
+        try (Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "mctoooo")) 
+        {
             if (conn == null) {
                 System.out.print("Failed to connect");
                 return;
             }
-String sql =
-                  " CREATE TABLE IF NOT EXISTS factory("
-                  + "factory_id SERIAL PRIMARY KEY,"
-                  + "factory_name TEXT ,"
-                  + "FACTORY_num int);";
-            Statement statement = conn.createStatement();
-            statement.execute(sql);
-            
+
             File file = new File("C:\\Users\\Дракоша Тоша\\Documents\\NetBeansProjects\\posgreSQL_txt_json_csv\\src\\main\\java\\com\\mycompany\\json_txt_csv\\Description.txt");
             Scanner sc = new Scanner(file);
-            System.out.println("file: " + "C:\\Users\\Дракоша Тоша\\Documents\\NetBeansProjects\\posgreSQL_txt_json_csv\\src\\main\\java\\com\\mycompany\\json_txt_csv\\Description.txt");
+            System.out.println("file: " + file);
             while (sc.hasNextLine()) {
                 System.out.println(sc.nextLine());
             }
             String fileName = "C:\\Users\\Дракоша Тоша\\Documents\\NetBeansProjects\\posgreSQL_txt_json_csv\\src\\main\\java\\com\\mycompany\\json_txt_csv\\description of fields.csv";
-            CSVReader reader = new CSVReader(new FileReader("C:\\Users\\Дракоша Тоша\\Documents\\NetBeansProjects\\posgreSQL_txt_json_csv\\src\\main\\java\\com\\mycompany\\json_txt_csv\\description of fields.csv")) {
-              String[] lineInArray;
-            {    
-            while((lineInArray = reader.readNext()) != null){
-          System.out.println(lineInArray[0] + lineInArray[1] + "etc...");
+            CSVReader reader = new CSVReader(new FileReader("fileName")) {
+
+                ArrayList TableCSV = new ArrayList<String[]>();
+                String[] lineInArray;
+
+                for(int i = 0;(lineInArray = reader.readNext()) != null; i++){
+                    TableCSV.add (lineInArray);
                 }
-            }
-            
-            
-        };
-                    }catch (SQLException ex) {
+
+                while((lineInArray  = reader.readNext()) != null)
+                {
+                    System.out.println(lineInArray[0] + lineInArray[1] + "etc...");
+                }
+            };
+        conn.close();
+        } catch (SQLException ex) 
+        {
             Logger.getLogger(Json_txt_csv.class.getName()).log(Level.SEVERE, null, ex);
         }
-       //conn.close(); 
+         
     }
 }
+
+
